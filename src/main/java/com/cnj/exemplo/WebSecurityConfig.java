63b95da516e4ed97258e02c3280c02caf029bb44
Jessica da Silva Santos Cunha <jessica.cunha@tjse.jus.br>
2025-06-27 13:15:13 -0300

    adiciona anotações para spring security, aula 06 parte 01

---
+++ src/main/java/com/cnj/exemplo/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.cnj.exemplo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("jessica").password("{noop}123").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}123").roles("ADMIN");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(new AntPathRequestMatcher("/api/casos/**"))
+                        .hasRole("USER"))
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(new AntPathRequestMatcher("/api/arquivos/**"))
+                        .hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+}
